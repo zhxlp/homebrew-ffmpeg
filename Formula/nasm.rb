94d34355ae5ec0f7b8578749e393a84e23cf58a2
zhxlp <1573635222@qq.com>
2021-08-22 13:08:27 +0800

    update nasm

--- Formula/nasm.rb
+++ Formula/nasm.rb
@@ -4,13 +4,6 @@ class Nasm < Formula
   url "https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz"
   sha256 "e24ade3e928f7253aa8c14aa44726d1edf3f98643f87c9d72ec1df44b26be8f5"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "77a183895137e0f95d897d33399235a58f1f23c99bac7f041d58baeb6f32e3b9" => :mojave
-    sha256 "b8d720e9008f58bbeddee14a8fa4841a8913c3d61d46872b37f7736d03a06218" => :high_sierra
-    sha256 "1d5283c366d958a1b7efdc22983fe313e0bd2a26268e585ced9f284f50ed270b" => :sierra
-  end
-
   head do
     url "https://repo.or.cz/nasm.git"
     depends_on "asciidoc" => :build
