a7abb7e3dbd380ecfaf84c5f6d008d1403a24237
zhxlp <1573635222@qq.com>
2021-08-22 15:41:35 +0800

    update java

---
+++ Formula/openjdk.rb
@@ -0,0 +1,33 @@
+class Openjdk < Formula
+  desc "Development kit for the Java programming language"
+  homepage "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/"
+  version "1.8"
+  url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz"
+  sha256 "5646fbe9e4138c902c910bb7014d41463976598097ad03919e4848634c7e8007"
+
+   
+  def install
+    on_macos do
+      jdk = Dir["*"].first
+      libexec.install jdk => "openjdk.jdk"
+      bin.install_symlink Dir[libexec/"openjdk.jdk/Contents/Home/bin/*"]
+      include.install_symlink Dir[libexec/"openjdk.jdk/Contents/Home/include/*.h"]
+      include.install_symlink Dir[libexec/"openjdk.jdk/Contents/Home/include/darwin/*.h"]
+      man1.install_symlink Dir[libexec/"openjdk.jdk/Contents/Home/man/man1/*"]
+    end
+  end
+
+  test do
+    (testpath/"HelloWorld.java").write <<~EOS
+      class HelloWorld {
+        public static void main(String args[]) {
+          System.out.println("Hello, world!");
+        }
+      }
+    EOS
+
+    system bin/"javac", "HelloWorld.java"
+
+    assert_match "Hello, world!", shell_output("#{bin}/java HelloWorld")
+  end
+end
