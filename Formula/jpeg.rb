e409a10e7e38099f824aebe1a906864df83c85b1
zhxlp <1573635222@qq.com>
2021-08-22 22:13:49 +0800

    update jpeg sha256

--- Formula/jpeg.rb
+++ Formula/jpeg.rb
@@ -4,7 +4,7 @@ class Jpeg < Formula
   url "https://www.ijg.org/files/jpegsrc.v9c.tar.gz"
   mirror "https://dl.bintray.com/homebrew/mirror/jpeg-9c.tar.gz"
   mirror "https://fossies.org/linux/misc/jpegsrc.v9c.tar.gz"
-  sha256 "650250979303a649e21f87b5ccd02672af1ea6954b911342ea491f351ceb7122"
+  sha256 "1e9793e1c6ba66e7e0b6e5fe7fd0f9e935cc697854d5737adec54d93e5b3f730"
 
 
 
